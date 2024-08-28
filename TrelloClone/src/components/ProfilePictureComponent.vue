67c153edd5a70f66eb7222f3cfd7ebe42fe1f4d3
RemiDebruyne <remidebruyne@hotmail.com>
2024-08-28 11:00:30 +0200

    feat: creae a profile picture component
    It takes the name of a member and displays its initials

---
+++ TrelloClone/src/components/ProfilePictureComponent.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { computed } from 'vue';
+
+ const props = defineProps({
+    name: String
+ })
+
+ const initials = computed(() => {
+    const splitName = props.name.split(' ')
+    return splitName.map(x => x.charAt(0).toUpperCase()).join('')
+ })
+</script>
+
+<template>
+    <div>{{ initials }}</div>
+</template>
+
+<style scoped>
+    div{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 12px;
+        font-weight: 500;
+        background-color: orange;
+        width: 24px;
+        height: 24px;
+        border-radius: 100%;
+    }
+</style>
