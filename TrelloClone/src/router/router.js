ed7186463df72656e257ea7c6707e47b4e28f2d6
RemiDebruyne <remidebruyne@hotmail.com>
2024-08-30 12:01:32 +0200

    update router

--- TrelloClone/src/router/router.js
+++ TrelloClone/src/router/router.js
@@ -1,18 +1,42 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import CardView from "../views/CardView.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import CardView from '../views/CardView.vue';
+import ListComponent from '@/components/ListComponent.vue';
+import TempTableComponent from '@/views/TempTableComponent.vue';
+import TagComponent from '@/components/TagsComponents/TagComponent.vue';
+import TagSelectorComponent from '@/components/TagsComponents/TagComponent.vue';
+import TagCreatorComponent from '@/components/TagsComponents/TagCreatorComponent.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/",
-      name: "home",
+      path: '/',
+      name: 'home',
       component: HomeView,
     },
     {
-      path: "/card",
+      path: '/card',
       component: CardView,
+    },
+    {
+      path: '/list',
+      component: ListComponent,
+    },
+    { path: '/table', 
+      component: TempTableComponent 
+    },
+    {
+      path: '/tags',
+      component: TagComponent,
+    },
+    {
+      path: '/tag-selector',
+      component: TagSelectorComponent,
+    },
+    {
+      path: '/tag-creator',
+      component: TagCreatorComponent,
     },
   ],
 });
